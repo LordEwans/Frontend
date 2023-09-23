cfd51251c934dd7eab1e9abfc29280b798eca0f7
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-09-24 00:06:36 +0100

    Update

--- pages/login.vue
+++ pages/login.vue
@@ -2,24 +2,16 @@
   <IonPage>
     <IonContent class="w-full h-full">
       <div class="flex items-center justify-center h-full">
-        <IonCard class="max-w-[600px]">
-          <IonRow>
-            <IonCol
-              size-md="3"
-              size="12"
-              class="bg-blue-800 ion-padding flex flex-col justify-center items-center max-sm:max-h-[200px]"
-            >
-              <img
-                src="https://images.prismic.io/ionicframeworkcom/ac68e1d9-9887-4e5a-9820-9290d06638de_ionic+logo+white+on+blue.png"
-                alt="Ionic logo"
-                loading="lazy"
-              />
-            </IonCol>
-            <IonCol size-md="9" size="12" class="welcome">
+        <IonCard class="max-w-[784px]">
+          <IonRow class="flex flex-row align-middle p-6">
+            <IonCol size-md="7" size="12" class="welcome">
               <IonCardHeader>
-                <IonCardTitle class="text-center font-bold text-3xl">Welcome back!</IonCardTitle>
-                <IonCardSubtitle class="text-center font-semibold uppercase text-xs"
-                  >We're so excited seeing you again!</IonCardSubtitle
+                <IonCardTitle class="text-center font-semi-bold text-2xl"
+                  >Welcome back!</IonCardTitle
+                >
+                <IonCardSubtitle
+                  class="text-center font-normal text-sm"
+                  >We're so excited to meet again!</IonCardSubtitle
                 >
               </IonCardHeader>
 
@@ -28,13 +20,21 @@
                   >Email or username
                   <span class="text-red-700">*</span>
                 </label>
-                <input type="email" class="input input-ghost w-full mb-4 h-10 focus:outline-none" />
+                <input
+                  type="email"
+                  class="input input-ghost w-full mb-4 h-10 focus:outline-none"
+                />
                 <label class="uppercase font-bold text-xs mt-2"
                   >Password
                   <span class="text-red-700">*</span>
                 </label>
-                <input type="password" class="input input-ghost w-full mb-1 h-10 focus:outline-none" />
-                <a href="http://" class="text-xs font-medium">Forgot your password?</a>
+                <input
+                  type="password"
+                  class="input input-ghost w-full mb-1 h-10 focus:outline-none"
+                />
+                <a href="http://" class="text-xs font-medium"
+                  >Forgot your password?</a
+                >
                 <IonButton
                   type="submit"
                   router-link="/signin"
@@ -46,6 +46,11 @@
                   New here? <a href="/register" class="font-medium">Register</a>
                 </span>
               </IonCardContent>
+            </IonCol>
+            <IonCol
+              size-md="5"
+              class="ion-padding flex flex-col justify-center items-center"
+            >
             </IonCol>
           </IonRow>
         </IonCard>
